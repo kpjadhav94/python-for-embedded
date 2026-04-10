744b811c00867b028eb9951d6847cb351e15ced1
Ketan Jadhav <erkpjadhav22@gmail.com>
2026-04-10 11:05:38 +0530

    Phase 1: Lesson 1: Hello World

---
+++ phase1_fundamentals/lesson1_hello_world.py
@@ -0,0 +1,7 @@
+# Program to print "Hello, World!" in Python
+# Python uses # for comments
+# print() is a built-in function in Python that outputs the specified message to the console
+# The message to be printed is enclosed in quotes (either single ' or double " quotes can be used)
+# Here we not using printf() as in C/C++ because Python has its own print() function for output
+
+print("Hello, World!")  # This line prints the message to the console.
