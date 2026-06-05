a6a12af90797f794fef21d9d85fada6d5e7fd412
Ketan Jadhav <erkpjadhav22@gmail.com>
2026-06-05 09:09:20 +0530

    4_lesson3_strings_in_python
    Added Example 2: String Indexing like C arrays. Also used Negative indexing - Python feature

--- phase1_fundamentals/lesson3_strings_in_python.py
+++ phase1_fundamentals/lesson3_strings_in_python.py
@@ -9,3 +9,21 @@ print ("""This is a         # Multi-line string example.
        multi-line
        string.""")
 
+# Example 2: String Indexing - Like C Arrays
+mcu = "STM32F4"
+
+# Indexing - exactly like C arrays, we can access individual 
+# characters in a string using their index.
+print(mcu[0])       # S
+print(mcu[1])       # T
+print(mcu[2])       # M
+
+# Negative indexing - unique to Python, we can also access characters from 
+# the end of the string using negative indices.
+print(mcu[-1])      # 4 <- Last character
+print(mcu[-2])      # F <- Second last character
+
+# In the above example, Memory Layout is looks like:
+# S    T      M      3      2      F      4
+# [0]  [1]    [2]    [3]    [4]    [5]    [6]   
+# [-7] [-6]   [-5]   [-4]   [-3]   [-2]   [-1]   
