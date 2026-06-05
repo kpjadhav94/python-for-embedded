0829b692b0bf79b53be4eabd5c9379685d17250a
Ketan Jadhav <erkpjadhav22@gmail.com>
2026-06-05 08:45:39 +0530

    4_Adding_String_Example_1
    This example explains about different ways to use string in python.

---
+++ phase1_fundamentals/lesson3_strings_in_python.py
@@ -0,0 +1,11 @@
+# Lines that start with a "#" are comments.
+
+# Example 1: Printing a string
+# In Python, we can use either single quotes ' ' or double quotes " " to define a string.
+# For multi-line strings, we can use triple quotes ''' ''' or """ """.
+print("Hello World!")       # Double quotes for a string
+print ('My name is Ketan')  # Single quotes for a string
+print ("""This is a         # Multi-line string example.
+       multi-line
+       string.""")
+
